d3b1be0d1a0b48ca544c55d5f332a7ea53abb8a4
anjalibhoge <anjalibhoge@gmail.com>
2020-09-30 00:49:37 +0530

    Changes

--- JavaAssignment/src/com/assignment/harshal/Demo.java
+++ JavaAssignment/src/com/assignment/harshal/Demo.java
@@ -3,7 +3,7 @@ package com.assignment.harshal;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World Thanks");
 
 	}
 
