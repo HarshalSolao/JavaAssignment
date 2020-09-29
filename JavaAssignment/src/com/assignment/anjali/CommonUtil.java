a976da7b50234e27195c46a09e80d15d139be51c
anjalibhoge <anjalibhoge@gmail.com>
2020-09-29 23:20:07 +0530

    Adding Interface and Class

---
+++ JavaAssignment/src/com/assignment/anjali/CommonUtil.java
@@ -0,0 +1,10 @@
+package com.assignment.anjali;
+
+public class CommonUtil implements CommonUtilInterface{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaAssignment/src/com/assignment/anjali/CommonUtilInterface.java
@@ -0,0 +1,5 @@
+package com.assignment.anjali;
+
+public interface CommonUtilInterface {
+
+}
