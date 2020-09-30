efc339f3d7bed208682d4984109ed757d353ae00
anjalibhoge <anjalibhoge@gmail.com>
2020-09-30 20:23:12 +0530

    Added new class and change in method

--- JavaAssignment/src/com/assignment/anjali/TestDemo.java
+++ JavaAssignment/src/com/assignment/anjali/TestDemo.java
@@ -1,12 +1,10 @@
 package com.assignment.anjali;
 
-public class Test {
+public class TestDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+
 	}
 
-	static void show() {
-	}
 }
