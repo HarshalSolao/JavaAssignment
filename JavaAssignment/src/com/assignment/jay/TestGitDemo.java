37f441a1270a52a84e4c767e3bcebecd6fb0f3c2
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2020-10-02 21:02:24 +0530

    Create new object

--- JavaAssignment/src/com/assignment/jay/TestGitDemo.java
+++ JavaAssignment/src/com/assignment/jay/TestGitDemo.java
@@ -4,7 +4,9 @@ public class TestGitDemo {
 
 	public static void main(String[] args) {
 		GitDemo gdemo = new GitDemo("Java","Amravati");
+		GitDemo gdemo1 = new GitDemo("Python","Pune");
 		System.out.println(gdemo);
+		System.out.println(gdemo1);
 
 	}
 
