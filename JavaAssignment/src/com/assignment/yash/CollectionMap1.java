a167c81d2a04aec37b9c864d6ed46b2bd43acb76
yashingole9881 <65347812+yashingole9881@users.noreply.github.com>
2020-10-07 16:35:19 +0530

    CollectionMap class is added

--- JavaAssignment/src/com/assignment/yash/CollectionMap1.java
+++ JavaAssignment/src/com/assignment/yash/CollectionMap1.java
@@ -20,6 +20,7 @@ public class CollectionMap1 {
 	 System.out.println(hs); 
 	 hs.remove("Rohit"); 
 	 System.out.println(hs); 
+	 System.out.println("Using student class");
 		HashMap<Student,String>hs1=new HashMap<>();
 	   Student s1=new Student(12,"Ram"); 
 	   Student s2=new Student(43,"xi"); 
