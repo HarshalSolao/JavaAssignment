9e7060c48c345813b527f786b2e64115428b3710
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2020-10-27 18:17:28 +0530

    JDBC Connection successful

---
+++ JavaAssignment/src/com/assignment/jay/JDBC_Connectionn.java
@@ -0,0 +1,43 @@
+package com.assignment.jay;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
+
+
+public class JDBC_Connectionn {
+
+	public static void main(String [] args) {
+		Connection con = null; 
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "root");
+
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from student");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getInt(4)
+						+ "  " + rs.getInt(5) + "  " + rs.getString(6) + "  " + rs.getString(7));
+			}
+			
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		
+	}
+	
+}
