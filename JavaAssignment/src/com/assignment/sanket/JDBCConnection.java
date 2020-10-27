9ea5358c99f623d2faadede90703ffd898e1439e
SanketMore1 <70767308+SanketMore1@users.noreply.github.com>
2020-10-27 18:48:59 +0530

    JDBC Class added.

--- JavaAssignment/src/com/assignment/sanket/JDBCConnection.java
+++ JavaAssignment/src/com/assignment/sanket/JDBCConnection.java
@@ -17,7 +17,7 @@ public class JDBCConnection {
 
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from student");
-			
+			System.out.println();
 			while (rs.next()) {
 				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getInt(4)
 						+ "  " + rs.getInt(5) + "  " + rs.getString(6) + "  " + rs.getString(7));
