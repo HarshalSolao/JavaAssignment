3ec4e816f11b8428b335c4289f3d9253bd7e5c67
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2020-10-27 18:19:37 +0530

    JDBC Connection successful

--- JavaAssignment/src/com/assignment/jay/JDBC_Connectionn.java
+++ JavaAssignment/src/com/assignment/jay/JDBC_Connectionn.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.UUID;
+import java.util.UUID; 
 
 
 public class JDBC_Connectionn {
