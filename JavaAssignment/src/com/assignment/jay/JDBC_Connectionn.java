b4e96b1ae166ac2746fe3456841971f16b67820e
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2020-10-28 21:47:15 +0530

    CRUD on JDBC Connection

--- JavaAssignment/src/com/assignment/jay/JDBC_Connectionn.java
+++ JavaAssignment/src/com/assignment/jay/JDBC_Connectionn.java
@@ -15,8 +15,8 @@ public class JDBC_Connectionn {
 		Connection con = null; 
 		try {
 			
-			Class.forName("com.mysql.cj.jdbc.Driver");  
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "root");
+			Class.forName("com.mysql.cj.jdbc.Driver");  //creation of driver object
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "root"); //creation of connection object
 
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from student");
@@ -26,7 +26,62 @@ public class JDBC_Connectionn {
 						+ "  " + rs.getInt(5) + "  " + rs.getString(6) + "  " + rs.getString(7));
 			}
 			
-
+			//Insert elements
+//			String sql = "INSERT INTO student (rollNo, firstName, lastName, age, city, state) VALUES (?,?,?,?,?,?)";
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setInt(1,105);
+//			ps.setString(2, "Jay");
+//			ps.setString(3, "Bhakte");
+//			ps.setInt(4,19);
+//			ps.setString(5, "Amravati");
+//			ps.setString(6, "mh");
+//			
+//			ps.setInt(1,106);
+//			ps.setString(2,"Sanket");
+//			ps.setString(3, "More");
+//			ps.setInt(4, 19);
+//			ps.setString(5, "Amravati");
+//			ps.setString(6,"MH");
+//		
+//			int b = ps.executeUpdate();
+//			if(b>0) {
+//				System.out.println("Added Successfully");
+//			}
+//			
+			//Update elements
+//			String sql = "UPDATE student SET firstName = ? , lastName = ? where id = ?";
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setString(1, "Yash");
+//			ps.setString(2, "Chopra");
+//			ps.setInt(3, 8);
+//			
+//			ps.setString(1,"Yash");
+//			ps.setString(2, "Ingole");
+//			ps.setInt(3, 4);
+//			
+//			int a = ps.executeUpdate();
+//			if (a>0) {
+//				System.out.println("UPDATED SUCCESFULLY");
+//			}
+			
+			
+			
+			
+			
+			
+			
+		//Delete elements
+//			String sql = "DELETE FROM student where id = ?";
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setInt(1, 7);
+//			
+//			ps.setInt(1,3);
+//			int a = ps.executeUpdate();
+//			if (a>0) {
+//				System.out.println("DELETED SUCCESFULLY");
+//			}
+			
+			
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		} finally {
